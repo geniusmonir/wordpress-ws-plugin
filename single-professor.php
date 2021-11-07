a641784bf82834774c195852e151141398d919b2
Md. Moniruzzaman <monir.jbt@gmail.com>
2021-11-08 04:51:34 +0600

    Added Page Banner Function

--- single-professor.php
+++ single-professor.php
@@ -3,16 +3,15 @@
   get_header();
 
   while(have_posts()) {
-    the_post(); ?>
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php $pagebannerImage = get_field('page_banner_background_image'); echo $pagebannerImage['sizes']['pageBanner'] ?>);"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p><?php the_field('page_banner_subtitle') ?></p>
-        </div>
-      </div>  
-    </div>
+    the_post(); 
+
+    pageBanner(array(
+      'title' => 'This is the Title',
+      'subtitle' => 'This is the subtitle'
+    ));
+    
+    ?>
+    
 
     <div class="container container--narrow page-section">
         
