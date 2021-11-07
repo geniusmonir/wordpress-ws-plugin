979ed7aece55b1036795f7783e04873ed8865146
Md. Moniruzzaman <monir.jbt@gmail.com>
2021-11-08 05:17:26 +0600

    Remove Duplicate Codes

--- front-page.php
+++ front-page.php
@@ -35,26 +35,11 @@
           ));
 
           while ($homePageEvents->have_posts()) {
-            $homePageEvents->the_post(); ?>
-
-            <div class="event-summary">
-              <a class="event-summary__date t-center" href="#">
-                <span class="event-summary__month"><?php 
-                  $eventData = new DateTime(get_field('event_date'));
-                  echo $eventData->format('M')
-                ?></span>
-                <span class="event-summary__day"><?php echo $eventData->format('d'); ?></span>  
-              </a>
-              <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                <p><?php if(has_excerpt()){
-                  echo get_the_excerpt();
-                }else{
-                  echo wp_trim_words(get_the_content(), 18);
-                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-              </div>
-            </div>
-         <?php  } wp_reset_postdata(); ?>
+            $homePageEvents->the_post(); 
+            
+            get_template_part('template-parts/content', 'event');
+            
+            } wp_reset_postdata(); ?>
         
        
         
--- single-program.php
+++ single-program.php
@@ -1,23 +1,15 @@
 <?php
-  
   get_header();
-
   while(have_posts()) {
     the_post();
-
     pageBanner();
-    
     ?>
-
     <div class="container container--narrow page-section">
-          <div class="metabox metabox--position-up metabox--with-home-link">
+      <div class="metabox metabox--position-up metabox--with-home-link">
         <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i>All Programs</a> <span class="metabox__main"><?php the_title() ?></span></p>
       </div>
-
       <div class="generic-content"><?php the_content(); ?></div>
-
        <?php 
-
         $relatedProfessors = new WP_Query(array(
             'posts_per_page' => -1,
             'post_type' => 'professor',
@@ -34,11 +26,8 @@
           ));
 
           if($relatedProfessors->have_posts()){
-             echo '<hr class="section-break">';
-
+              echo '<hr class="section-break">';
               echo '<h2 class="headline headline--medium" >' . get_the_title() . ' Professors</h2>';
-
-
               echo '<ul class="professor-cards">';
               while ($relatedProfessors->have_posts()) {
                 $relatedProfessors->the_post(); ?>
@@ -53,9 +42,6 @@
             <?php  } wp_reset_postdata(); 
             echo '</ul>';
           }
-
-
-
 
           /// XXX
           $today = date('Ymd');
@@ -82,39 +68,15 @@
           ));
 
           if($homePageEvents->have_posts()){
-             echo '<hr class="section-break">';
-
+              echo '<hr class="section-break">';
               echo '<h2 class="headline headline--medium" >Upcoming ' . get_the_title() . ' Events</h2>';
-
               while ($homePageEvents->have_posts()) {
-                $homePageEvents->the_post(); ?>
-
-                <div class="event-summary">
-                  <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month"><?php 
-                      $eventData = new DateTime(get_field('event_date'));
-                      echo $eventData->format('M')
-                    ?></span>
-                    <span class="event-summary__day"><?php echo $eventData->format('d'); ?></span>  
-                  </a>
-                  <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                    <p><?php if(has_excerpt()){
-                      echo get_the_excerpt();
-                    }else{
-                      echo wp_trim_words(get_the_content(), 18);
-                    } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                  </div>
-                </div>
-            <?php  } wp_reset_postdata(); 
+                $homePageEvents->the_post();
+                get_template_part('template-parts/content-event');
+              } wp_reset_postdata(); 
           }
-
          ?>
-
     </div>
-    
-
-    
   <?php }
 
   get_footer();
