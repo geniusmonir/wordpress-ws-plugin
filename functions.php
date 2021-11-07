2863a872b80e14dbb8fcbbf309d3470f094beaa3
Md. Moniruzzaman <monir.jbt@gmail.com>
2021-11-07 18:15:41 +0600

    Added Menu others

--- functions.php
+++ functions.php
@@ -12,6 +12,8 @@ add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features() {
   register_nav_menu('headerMenuLocation','Header Menu Location');
+  register_nav_menu('footerLocationOne','Footer Menu One');
+  register_nav_menu('footerLocationTwo','Footer Menu Two');
   add_theme_support('title-tag');
 }
 
