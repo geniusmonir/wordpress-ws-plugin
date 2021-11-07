01830d4ae4e88e515d124170a82334bcfd4a50d1
Md. Moniruzzaman <monir.jbt@gmail.com>
2021-11-08 03:18:36 +0600

    Added Professor with Relation

--- single-program.php
+++ single-program.php
@@ -22,6 +22,38 @@
       <div class="generic-content"><?php the_content(); ?></div>
 
        <?php 
+
+        $relatedProfessors = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => 'professor',
+            // 'meta_key' => 'event_date',
+            'orderby' => 'title',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"',
+              )
+            )
+          ));
+
+          if($relatedProfessors->have_posts()){
+             echo '<hr class="section-break">';
+
+              echo '<h2 class="headline headline--medium" >' . get_the_title() . ' Professors</h2>';
+
+              while ($relatedProfessors->have_posts()) {
+                $relatedProfessors->the_post(); ?>
+
+                  <li><a href="<?php the_permalink() ?>"><?php the_title() ?></a></li>
+            <?php  } wp_reset_postdata(); 
+          }
+
+
+
+
+          /// XXX
           $today = date('Ymd');
 
           $homePageEvents = new WP_Query(array(
