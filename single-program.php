f5a2ed281d6721287e8f057d21c7fabf1f313250
Md. Moniruzzaman <monir.jbt@gmail.com>
2021-11-08 04:08:22 +0600

    Optimized Image

--- single-program.php
+++ single-program.php
@@ -43,11 +43,20 @@
 
               echo '<h2 class="headline headline--medium" >' . get_the_title() . ' Professors</h2>';
 
+
+              echo '<ul class="professor-cards">';
               while ($relatedProfessors->have_posts()) {
                 $relatedProfessors->the_post(); ?>
 
-                  <li><a href="<?php the_permalink() ?>"><?php the_title() ?></a></li>
+                  <li class="professor-card__list-item">
+                    <a class="professor-card" href="<?php the_permalink() ?>">
+
+                      <img src="<?php the_post_thumbnail_url('professorLandscape') ?>" alt="" class="professor-card__image">
+                      <span class="professor-card__name"><?php the_title() ?></span>
+                    </a>
+                  </li>
             <?php  } wp_reset_postdata(); 
+            echo '</ul>';
           }
 
 
