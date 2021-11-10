c9cfbc942a0bd2efc6c48c99a74171dfe3e433ec
Md. Moniruzzaman <monir.jbt@gmail.com>
2021-11-10 07:00:30 +0600

    Login Page Customization

--- functions.php
+++ functions.php
@@ -132,3 +132,27 @@ function noSubsAdminBar(){
 }
 
 add_action('wp_loaded', 'noSubsAdminBar');
+
+
+// Customize login Screen
+function ourHeaderUrl(){
+  return esc_url(site_url('/'));
+}
+
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function my_login_logo_url_title() {
+    return get_bloginfo('name');
+}
+add_filter( 'login_headertext', 'my_login_logo_url_title' ); 
+// add_filter( 'login_headertitle', 'my_login_logo_url_title' ); //same
+
+
+function ourLoginCSS(){
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
